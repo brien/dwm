b33eb3cdbf94f55d5c0e69663436e7a47598239e
Brien Smith-Martinez <bsmartinez@gmail.com>
2013-04-07 00:10:46 -0500

    Added some rules

--- config.h
+++ config.h
@@ -22,10 +22,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 2,       False,       -1 },
-	{ "Iceweasel",NULL,       NULL,       1 << 2,       False,       -1 },
-	{ "Skype",    NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Gimp",       NULL,       NULL,       0,            True,        -1 },
+	{ "Firefox",    NULL,       NULL,       1 << 2,       False,       -1 },
+	{ "Iceweasel",  NULL,       NULL,       1 << 2,       False,       -1 },
+	{ "Skype",      NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "Icedove",    NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Thunderbird",NULL,       NULL,       1 << 3,       False,       -1 },
 };
 
 /* layout(s) */
